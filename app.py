b195bead1600dae1e4d3194ed3522ff64c9c9bd9
Sundaresan C G <73348349+sundaresancg@users.noreply.github.com>
2023-10-25 11:00:33 +0530

    Update app.py

--- app.py
+++ app.py
@@ -6,10 +6,10 @@ app = Flask(__name__)
 def strlen():
     data = request.json
     print(data)
-    string = data.get("input")
-    count = len(string)
-    print(string,"=",count)
-    return jsonify({"length": count})
+    #string = data.get("input")
+    #count = len(string)
+    #print(string,"=",count)
+    #return jsonify({"length": count})
 
 if __name__ == '__main__':
     app.run(debug=True)
